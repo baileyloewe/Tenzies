8d2c317da9c159860b49eca3cc7c5477e0a8f013
Bailey Loewe <72360204+baileyloewe@users.noreply.github.com>
2025-09-02 19:08:11 -0400

    Adjusted layout to use grid per requirements

--- App.jsx
+++ App.jsx
@@ -2,21 +2,15 @@ import Dice from './components/Die.jsx'
 
 export default function App() {
 
-    const diceValues1 = new Array(5).fill().map(() => Math.floor(Math.random() * 6))
-    const diceValues2 = new Array(5).fill().map(() => Math.floor(Math.random() * 6))
+    const diceValues = new Array(10).fill().map(() => Math.floor(Math.random() * 6))
 
-    const diceComponentsRow1 = diceValues1.map(val => {
-        return <Dice num={val}/>
-    })
-
-    const diceComponentsRow2 = diceValues2.map(val => {
+    const diceComponents = diceValues.map(val => {
         return <Dice num={val}/>
     })
 
     return (
         <main>
-            <div>{diceComponentsRow1}</div>
-            <div>{diceComponentsRow2}</div>
+            <div className="dice-container">{diceComponents}</div>
         </main>
     )
 }
