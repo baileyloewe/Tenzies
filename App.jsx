3a9fedf802f1b0aeced41a6fd998b3a6c59f4f4a
Bailey Loewe <72360204+baileyloewe@users.noreply.github.com>
2025-09-02 19:28:22 -0400

    Mild adjustments to state and css

--- App.jsx
+++ App.jsx
@@ -1,16 +1,24 @@
 import Dice from './components/Die.jsx'
+import { useState } from 'react'
 
 export default function App() {
 
-    const diceValues = new Array(10).fill().map(() => Math.ceil(Math.random() * 6))
 
-    const diceComponents = diceValues.map(val => {
-        return <Dice num={val}/>
+    function generateAllNewDice() {
+        return new Array(10).fill().map(() => Math.ceil(Math.random() * 6))
+    }
+
+    const [dice, setDice] = useState(generateAllNewDice())
+
+    const diceElements = dice.map(num => {
+        return <Dice value={num}/>
     })
 
     return (
         <main>
-            <div className="dice-container">{diceComponents}</div>
+            <div className="dice-container">
+                {diceElements}
+            </div>
         </main>
     )
 }
