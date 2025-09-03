1e09c2d0f711828baaee68bc09ef8b1ddcdfaad9
Bailey Loewe <72360204+baileyloewe@users.noreply.github.com>
2025-09-03 10:17:59 -0400

    Implemented die holding

--- App.jsx
+++ App.jsx
@@ -1,25 +1,40 @@
 import Dice from './components/Die.jsx'
 import { useState } from 'react'
+import { nanoid } from "nanoid"
 
 export default function App() {
 
-
-    function generateAllNewDice() {
-        return new Array(10).fill().map(() => Math.ceil(Math.random() * 6))
-    }
-
     const [dice, setDice] = useState(generateAllNewDice())
 
-    const diceElements = dice.map(num => {
-        return <Dice value={num}/>
+    const diceElements = dice.map(die => {
+        return <Dice key={die.id} id={die.id} value={die.value} isHeld={die.isHeld} handleClick={handleClick}/>
     })
+
+    function handleClick(id) {
+        setDice(prevDice => prevDice.map(die => die.id === id ? {...die, isHeld: true} : {...die}))
+    }
+
+    function generateAllNewDice() {
+        return new Array(10)
+            .fill(0)
+            .map(() => ({
+                value: Math.ceil(Math.random() * 6), 
+                isHeld: false,
+                id: nanoid()
+            }))
+    }
+
+    function rollNewDice() {
+        setDice(prevDice =>  prevDice.map(die => !die.isHeld ? {...die, value: Math.ceil(Math.random() * 6)} : {...die}))
+    }
+    
 
     return (
         <main>
             <div className="dice-container">
                 {diceElements}
             </div>
-            <button className="roll-button" onClick={() => setDice(generateAllNewDice())}>Roll</button>
+            <button className="roll-button" onClick={rollNewDice}>Roll</button>
         </main>
     )
 }
--- components/Die.jsx
+++ components/Die.jsx
@@ -1,6 +1,6 @@
 export default function Die(props) {
     return (
-        <button className="dice">
+        <button className={`dice ${props.isHeld ? "held" : null}`} onClick={() => props.handleClick(props.id)}>
             {props.value}
         </button>
     )
